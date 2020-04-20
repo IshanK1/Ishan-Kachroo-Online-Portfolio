33f326f850b2c47250065bd4b81972dafb227648
IshanK1 <55854765+IshanK1@users.noreply.github.com>
2020-04-19 17:40:39 -0700

    Added all files from Repl upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<!-- The code provides basic metadata for this Personal Home page. NOTE: All Comments on ALL pages are placed above the code they explain.-->
+<html>
+<head>
+  <title>PHP Test</title>
+</head>
+<!--This code displays the text "Welcome To My website! -Ishan Kachroo" and also displays a clickable "Home" link. The code: <nav class="nav-tabs" may seem extraneous at first glance. However, It is actually used to "line break" the home button without leaving a line of space between "Welcome To My website! -Ishan Kachroo" and the home button. The Home link takes users to my index, where they can start viewing the website's content.  -->
+<body>
+  <div class="container" > Welcome To My Website! -Ishan Kachroo
+    <nav class="nav-tabs"><a href="index.html"> Home </nav> 
+  </div>
+  <!-- This code above displays a bulleted list of links for the user to click on if they would like to skip to one of my webpages directly. Also, users can still hop between pages if I am in the process of changing the the navbar present in all the other pages.-->
+  <ul>
+    <li><a href="index.html">Index</a></li></br>
+    <li><a href="Kachroo_AboutMe.html">About Me </a></li></br>
+    <li><a href="Kachroo_Projects.html">Projects </a></li></br>
+    <li><a href="Kachroo_Resume.html">My Resume</a></li></br>
+    <li><a href="Kachroo_ContactMe.html">Contact Me</a></li></br>
+  <ul>
+</body>
+</html>
